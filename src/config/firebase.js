896d0e34128819a4fcc93b4855c3e414421b1ccd
NOMANRAHIM77 <nomanrahim53@gmail.com>
2025-12-22 22:04:27 +0500

    initilized project

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFireStore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA70Qj9bVA417RRRxGpEudVTz5m2jH7Bu0",
+  authDomain: "contact-app-c2891.firebaseapp.com",
+  projectId: "contact-app-c2891",
+  storageBucket: "contact-app-c2891.firebasestorage.app",
+  messagingSenderId: "789144862808",
+  appId: "1:789144862808:web:9e15355abc7135a6ddca2d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db=getFireStore(app)
